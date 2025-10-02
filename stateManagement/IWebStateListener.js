d777a42c1a0ce6bf862c1e2c6c4120abbb4ca15b
Lindsay Marín Sánchez <168288561+CholiRat@users.noreply.github.com>
2025-10-01 22:25:34 -0600

    Delete stateManagement directory

--- stateManagement/IWebStateListener.js
+++
@@ -1,8 +0,0 @@
-class IWebStateListener {
-
-    update(webState) {
-        throw new Error('update(webState) method must be implemented by subclass');
-    }
-}
-
-export default IWebStateListener;
